8352d210a1b908ef05719a87ee238cb0a1146d2e
Admin <Admin@192.168.100.11>
2018-12-01 20:14:04 +0530

    Fire added

--- PageObjectModel/src/com/abc/magentotest/MagentoTest.java
+++ PageObjectModel/src/com/abc/magentotest/MagentoTest.java
@@ -3,7 +3,7 @@ package com.abc.magentotest;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.abc.magentoobject.Login;
 import com.abc.magentoobject.Logout;
@@ -13,7 +13,7 @@ public class MagentoTest {
 
 	public static void main(String[] args) 
 	{
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://www.magento.com");
